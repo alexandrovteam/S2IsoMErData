cb3be3060d8109295dfaf6fee55b6c06810471a8
Bishoy Wadie <bwadie298@gmail.com>
2025-02-26 15:19:03 +0100

    First commit S2IsoMErData

---
+++ NEWS.md
@@ -0,0 +1,30 @@
+DataVersion: 0.1.0
+=======================
+* Added: exact_masses
+* Added: example_MSEA_multicond
+* Added: example_MSEA_obj
+* Added: example_ORA_custom_universe
+* Added: example_ORA_markers
+* Added: example_ORA_obj
+* Added: LION_LUT
+* Added: LipidMaps_category_name
+* Added: LipidMaps_category_sf
+* Added: LipidMaps_main_class_name
+* Added: LipidMaps_main_class_sf
+* Added: LipidMaps_pathway_name
+* Added: LipidMaps_pathway_sf
+* Added: LipidMaps_sub_class_name
+* Added: LipidMaps_sub_class_sf
+* Added: Metabo_class_name
+* Added: Metabo_class_sf
+* Added: Metabo_pathway_name
+* Added: Metabo_pathway_sf
+* Added: Metabo_sub_class_name
+* Added: Metabo_sub_class_sf
+* Added: Metabo_super_class_name
+* Added: Metabo_super_class_sf
+* Added: metaspace_databases
+* Added: pathway_list_LION
+* Added: termsSelection
+Added enrichmed background, isomers data, LION lipid sets and S2IsoMEr example data
+
---
+++ data-raw/documentation.R
@@ -0,0 +1,341 @@
+#' S2IsoMErData
+#' A data package for S2IsoMErData.
+#' @aliases S2IsoMErData-package
+#' @title Package Title
+#' @name S2IsoMErData
+#' @description A description of the data package
+#' @details Use \code{data(package='S2IsoMErData')$results[, 3]} to see a list of available data sets in this data package
+#'     and/or DataPackageR::load_all
+#' _datasets() to load them.
+'_PACKAGE'
+
+
+#' Detailed description of the data
+#' @name exact_masses
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{data.table} containing the following fields:
+#' \describe{
+#' \item{formula}{}
+#' \item{posH}{}
+#' \item{posNa}{}
+#' \item{posK}{}
+#' \item{negH}{}
+#' \item{negCl}{}
+#' }
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name example_MSEA_multicond
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{tbl_df} containing the following fields:
+#' \describe{
+#' \item{Term}{}
+#' \item{n}{}
+#' \item{ES_median}{}
+#' \item{ES_sd}{}
+#' \item{p.value_combined}{}
+#' \item{q.value_combined}{}
+#' \item{fraction.bootstrap.presence}{}
+#' \item{condition.x}{}
+#' \item{condition.y}{}
+#' }
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name example_MSEA_obj
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{S2IsoMEr} containing the following fields:
+#' \describe{
+#' \item{scmatrix}{}
+#' \item{polarization_mode}{}
+#' \item{enrichment_type}{}
+#' \item{Annotation_database}{}
+#' \item{Custom_database}{}
+#' \item{annotations}{}
+#' \item{annotation.weights}{}
+#' \item{isobars_list}{}
+#' \item{conditions}{}
+#' \item{include}{}
+#' \item{consider_isomers}{}
+#' \item{consider_isobars}{}
+#' \item{mass_range_ppm}{}
+#' \item{background_name}{}
+#' \item{endogenous_only}{}
+#' \item{pathway_assoc_only}{}
+#' \item{remove_expected_predicted}{}
+#' \item{pathway_list}{}
+#' \item{LUT}{}
+#' \item{termsSelection}{}
+#' \item{condition.x}{}
+#' \item{condition.y}{}
+#' \item{ranking.by}{}
+#' \item{gsea.method}{}
+#' \item{rankings}{}
+#' \item{enrichment_analysis}{}
+#' }
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name example_ORA_custom_universe
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{character} containing the following fields:
+#' \describe{
+#' }
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name example_ORA_markers
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' \describe{
+#' \item{upregulated}{}
+#' \item{downregulated}{}
+#' \item{all}{}
+#' }
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name example_ORA_obj
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{S2IsoMEr} containing the following fields:
+#' \describe{
+#' \item{scmatrix}{}
+#' \item{polarization_mode}{}
+#' \item{enrichment_type}{}
+#' \item{Annotation_database}{}
+#' \item{Custom_database}{}
+#' \item{annotations}{}
+#' \item{annotation.weights}{}
+#' \item{isobars_list}{}
+#' \item{conditions}{}
+#' \item{include}{}
+#' \item{consider_isomers}{}
+#' \item{consider_isobars}{}
+#' \item{mass_range_ppm}{}
+#' \item{background_name}{}
+#' \item{endogenous_only}{}
+#' \item{pathway_assoc_only}{}
+#' \item{remove_expected_predicted}{}
+#' \item{pathway_list}{}
+#' \item{LUT}{}
+#' \item{termsSelection}{}
+#' \item{condition.x}{}
+#' \item{condition.y}{}
+#' \item{ranking.by}{}
+#' \item{gsea.method}{}
+#' }
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name LION_LUT
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{data.table} containing the following fields:
+#' \describe{
+#' \item{ID}{}
+#' \item{name}{}
+#' }
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name LipidMaps_category_name
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name LipidMaps_category_sf
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name LipidMaps_main_class_name
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name LipidMaps_main_class_sf
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name LipidMaps_pathway_name
+#' @docType data
+#' @title Descriptive data title
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name LipidMaps_pathway_sf
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name LipidMaps_sub_class_name
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name LipidMaps_sub_class_sf
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name Metabo_class_name
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name Metabo_class_sf
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name Metabo_pathway_name
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name Metabo_pathway_sf
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name Metabo_sub_class_name
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name Metabo_sub_class_sf
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name Metabo_super_class_name
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name Metabo_super_class_sf
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name metaspace_databases
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{data.table} containing the following fields:
+#' \describe{
+#' \item{formula}{}
+#' \item{name}{}
+#' \item{db}{}
+#' \item{Endogenous}{}
+#' \item{Pathway_assoc}{}
+#' \item{HMDB_status}{}
+#' }
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name pathway_list_LION
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{list} containing the following fields:
+#' @source The data comes from________________________.
+NULL
+
+
+#' Detailed description of the data
+#' @name termsSelection
+#' @docType data
+#' @title Descriptive data title
+#' @format a \code{data.frame} containing the following fields:
+#' \describe{
+#' \item{LION_ID}{}
+#' \item{LION_name}{}
+#' }
+#' @source The data comes from________________________.
+NULL
+
+
